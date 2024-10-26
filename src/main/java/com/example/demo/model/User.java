29a94781a12472c31f78df78f5ace5c37c30963b
Yashasvi Sharma <142658623+Yashasvi213@users.noreply.github.com>
2024-10-26 12:50:41 +0530

    Update User.java

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -10,10 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
+
 public class User {
 	private String userid;
 	private String name;
